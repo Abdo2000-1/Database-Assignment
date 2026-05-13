b1a9e087f57d2ddb192aed621e7c3bd54186ad9c
yhany0126-dotcom <yhany0126@gmail.com>
2026-05-13 15:12:53 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaProductionManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
